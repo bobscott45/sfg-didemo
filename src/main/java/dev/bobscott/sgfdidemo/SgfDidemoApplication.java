f06d6b379c5109c88125d618697a7a2cd36e5ec8
Bob Scott <bob@rwscott.co.uk>
2019-03-31 17:16:32 +0100

    Spring Boot Project Creation

---
+++ src/main/java/dev/bobscott/sgfdidemo/SgfDidemoApplication.java
@@ -0,0 +1,13 @@
+package dev.bobscott.sgfdidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgfDidemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SgfDidemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/bobscott/sgfdidemo/SgfDidemoApplicationTests.java
@@ -0,0 +1,16 @@
+package dev.bobscott.sgfdidemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SgfDidemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
