f1574a88609ca09446a4ca4c7d0d923caca58f6e
Bob <bob@rwscott.co.uk>
2019-04-01 10:27:12 +0100

    Add PrimaryGreetingService

--- src/main/java/dev/bobscott/didemo/controllers/MyController.java
+++ src/main/java/dev/bobscott/didemo/controllers/MyController.java
@@ -1,12 +1,20 @@
 package dev.bobscott.didemo.controllers;
 
+import dev.bobscott.didemo.services.GreetingService;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MyController {
+    private GreetingService greetingService;
+
+
+    public MyController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
 
     public String hello() {
         System.out.println("Hello from Spring Boot!");
+        System.out.println(greetingService.sayGreeting());
         return "foo";
     }
 }
